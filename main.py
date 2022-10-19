c3efddabec08111c9135111885c107450123e269
yamanko.d <yamankodmitry@gmail.com>
2022-10-19 21:18:16 +0300

    UPD: working

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from collections import Counter
+import re
 import nltk
-import csv
-nltk.download('averaged_perceptron_tagger')
+
 # working url
 url = "https://www.w3.org/DesignIssues/TimBook-old/History.html"
 # load site code
@@ -10,8 +11,15 @@ html = urlopen(url)
 bs = BeautifulSoup(html.read(), 'html.parser')
 # get text from html
 text = bs.getText()
+# clear text
+text = re.sub('[!@#$:\-?...]', '', text)
 # get text tokens
 tokens = nltk.word_tokenize(text)
 # pos tokens
 tagged = nltk.pos_tag(tokens)
-print(tagged)
+# get list of tags
+tags_list = []
+for tag in tagged:
+    tags_list.append(tag[1])
+# print result(using counter to count tags usage)
+print(Counter(tags_list))
